7544fdc56b4265f5cc64d137b12cf6f8ba6bfcd3
Leonid <D067044@WDFN33937799A.emea.global.corp.sap>
2016-10-06 13:44:04 +0200

    COIN-2486 Wishlist: Update to Media v2

--- src/main/java/com/sap/wishlist/media/MediaClientService.java
+++ src/main/java/com/sap/wishlist/media/MediaClientService.java
@@ -15,55 +15,50 @@ import org.springframework.beans.factory.annotation.Value;
 import com.sap.cloud.yaas.servicesdk.authorization.AccessToken;
 import com.sap.cloud.yaas.servicesdk.patternsupport.schemas.ResourceLocation;
 import com.sap.wishlist.api.generated.YaasAwareParameters;
-import com.sap.wishlist.client.media.MediaClient;
+import com.sap.wishlist.client.media.SecureMediaClient;
 import com.sap.wishlist.utility.ErrorHandler;
-
 
 @ManagedBean
 public class MediaClientService {
 
-	@Inject
-	private MediaClient mediaClient;
+    @Inject
+    private SecureMediaClient mediaClient;
 
-	@Value("${YAAS_CLIENT}")
-	private String client;
+    @Value("${YAAS_CLIENT}")
+    private String client;
 
-	public ResourceLocation createMedia(final YaasAwareParameters yaasAware, final InputStream fileInputStream,
-			final AccessToken token) {
+    public ResourceLocation createMedia(final YaasAwareParameters yaasAware, final InputStream fileInputStream,
+            final AccessToken token) {
 
-		final FormDataMultiPart multiPart = new FormDataMultiPart();
-		multiPart.bodyPart(new StreamDataBodyPart("file", fileInputStream));
+        final FormDataMultiPart multiPart = new FormDataMultiPart();
+        multiPart.bodyPart(new StreamDataBodyPart("file", fileInputStream));
 
-		final Response response = mediaClient
-				.tenant(yaasAware.getHybrisTenant())
-				.client(client)
-				.media()
-				.preparePost()
-				.withAuthorization(token.toAuthorizationHeaderValue())
-				.withPayload(multiPart, MediaType.MULTIPART_FORM_DATA)
-				.execute();
+        final Response response = mediaClient
+                .files()
+                .preparePost()
+                .withAuthorization(token.toAuthorizationHeaderValue())
+                .withPayload(multiPart, MediaType.MULTIPART_FORM_DATA)
+                .execute();
 
-		if (response.getStatus() == Response.Status.CREATED.getStatusCode()) {
-			return response.readEntity(ResourceLocation.class);
-		}
+        if (response.getStatus() == Response.Status.CREATED.getStatusCode()) {
+            return response.readEntity(ResourceLocation.class);
+        }
 
-		throw ErrorHandler.resolveErrorResponse(response, token);
-	}
+        throw ErrorHandler.resolveErrorResponse(response, token);
+    }
 
-	public void deleteMedia(final YaasAwareParameters yaasAware, final String id, final AccessToken token) {
-		final Response response = mediaClient
-				.tenant(yaasAware.getHybrisTenant())
-				.client(client)
-				.media()
-				.mediaId(id)
-				.prepareDelete()
-				.withAuthorization(token.toAuthorizationHeaderValue())
-				.execute();
+    public void deleteMedia(final YaasAwareParameters yaasAware, final String id, final AccessToken token) {
+        final Response response = mediaClient
+                .files()
+                .fileId(id)
+                .prepareDelete()
+                .withAuthorization(token.toAuthorizationHeaderValue())
+                .execute();
 
-		if (response.getStatus() == Response.Status.NO_CONTENT.getStatusCode()) {
-			return;
-		}
+        if (response.getStatus() == Response.Status.NO_CONTENT.getStatusCode()) {
+            return;
+        }
 
-		throw ErrorHandler.resolveErrorResponse(response, token);
-	}
+        throw ErrorHandler.resolveErrorResponse(response, token);
+    }
 }
